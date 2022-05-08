6db9241e4599e5969995f726e45cec3bb2dfe142
Önder Görmez <ondergormez@gmail.com>
2022-05-08 20:11:15 +0300

    Add Person class

---
+++ 03-Project/Person.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+namespace LanguageSchool
+{
+    class Person
+    {
+    protected:
+        std::string name;
+        std::string surname;
+        std::string mobilephone;
+        std::string phone;
+        std::string email;
+    };
+} // LanguageSchool
