bc5a49407494a862c19ce936c26fb231807f1a12
Önder Görmez <ondergormez@gmail.com>
2022-05-08 20:18:05 +0300

    Add SchoolBranch class

---
+++ 03-Project/SchoolBranch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace LanguageSchool
+{
+    class ClassRoom
+    {
+        // TODO: Maybe move this
+    };
+
+    class SchoolBranch
+    {
+        std::string name;
+        std::string address;
+        // M2 metro, T34, 500T bus
+        std::string publicTransport;
+        // Use 15 Temmuz Bridge
+        std::string privateTransport;
+        // table tennis, langırt, ...
+        std::vector<std::string> socialBenefits;
+        std::vector<ClassRoom> allClasses;
+    };
+} // LanguageSchool
