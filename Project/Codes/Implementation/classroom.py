f33531a6811250f29706ff3244bf72c025239c95
Batuhan <batuhanhangun@gmail.com>
2022-05-19 20:19:18 +0300

    Code was modified

--- Project/Codes/Implementation/classroom.py
+++ Project/Codes/Implementation/classroom.py
@@ -1,8 +1,17 @@
 from student import Student
+from course import Course
 
 class Classroom():
     
     def __init__(self, name, capacity: int) -> None:
         self.name = name
         self.capacity = capacity
+    
+    def __del__(self):
+        f_str1 = f"{self.name}"
+        print('Classroom' + f_str1 + ' was deleted')      
         
+    course_list = []
+        
+    def add_course(self, course: Course):
+        self.course_list.append(course)
